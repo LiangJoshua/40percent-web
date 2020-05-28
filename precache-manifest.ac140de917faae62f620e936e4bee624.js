5137a6eae6ffd91d9ccfb956478b74ba9fb9f4db
Joshua Liang <joshuali@zillowgroup.com>
2020-05-27 17:26:54 -0700

    Updates

---
+++ precache-manifest.ac140de917faae62f620e936e4bee624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e7647824631c6dee3df9f8928acf26",
+    "url": "/calisthantics-web/index.html"
+  },
+  {
+    "revision": "cd3d14247eb6ea97c721",
+    "url": "/calisthantics-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b52e568f7628c96111a",
+    "url": "/calisthantics-web/static/js/2.13ae9985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calisthantics-web/static/js/2.13ae9985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3d14247eb6ea97c721",
+    "url": "/calisthantics-web/static/js/main.60600d00.chunk.js"
+  },
+  {
+    "revision": "62b63d4d1d20cdc8594c",
+    "url": "/calisthantics-web/static/js/runtime-main.978b2c6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calisthantics-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
